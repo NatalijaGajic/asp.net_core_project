a61465d3b61fef0c7b74ad1e1982fe8ef244c6b3
NatalijaGajic <nat.gaj98@gmail.com>
2022-10-22 15:50:41 +0200

    Sonar init

--- UserService/Startup.cs
+++ UserService/Startup.cs
@@ -39,7 +39,7 @@ namespace UserService
         }
 
         public IConfiguration Configuration { get; }
-
+		//Sonar
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
